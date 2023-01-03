01217e7966152b241a6259ade702a894bfee83a9
Domen Kožar <domen@dev.si>
2023-01-03 13:31:30 +0000

    switch back to hosted and enable experimental commands

--- agents/linux.nix
+++ agents/linux.nix
@@ -4,6 +4,9 @@
   system.stateVersion = "22.11";
 
   nix.settings.trusted-users = [ "root" "github-runner-cachix" ];
+  nix.extraOptions = ''
+     extra-experimental-features = flakes nix-command
+  '';
 
   systemd.services.github-runner-cachix.serviceConfig.ReadWritePaths = [ "/nix/var/nix/profiles/per-user/" ];
 
