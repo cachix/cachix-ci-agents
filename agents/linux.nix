68fea44da49026a325c46785f9587696001e8c1f
Domen Kožar <domen@dev.si>
2023-01-12 13:53:28 +0000

    Rename linux runner to cachix-linux

--- agents/linux.nix
+++ agents/linux.nix
@@ -9,14 +9,14 @@
 
   system.stateVersion = "22.11";
 
-  nix.settings.trusted-users = [ "root" "github-runner-cachix" ];
+  nix.settings.trusted-users = [ "root" "github-runner-cachix-linux" ];
   nix.extraOptions = ''
      extra-experimental-features = flakes nix-command
   '';
 
-  systemd.services.github-runner-cachix.serviceConfig.ReadWritePaths = [ "/nix/var/nix/profiles/per-user/" ];
+  systemd.services.github-runner-cachix-linux.serviceConfig.ReadWritePaths = [ "/nix/var/nix/profiles/per-user/" ];
 
-  services.github-runners.cachix = {
+  services.github-runners.cachix-linux = {
     enable = true;
     url = "https://github.com/cachix";
     tokenFile = "/etc/secrets/github-runner/cachix.token";
