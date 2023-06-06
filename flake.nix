5bdf2bb166b5e59c82018076dbd90be89e1f7369
Domen Kožar <domen@dev.si>
2023-06-06 12:15:14 +0100

    dump nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     cachix-deploy-flake.url = "github:cachix/cachix-deploy-flake";
     devenv.url = "github:cachix/devenv/v0.6";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, devenv, nixpkgs, cachix-deploy-flake, ... }: 
