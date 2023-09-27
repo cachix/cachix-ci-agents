09afcc3c6239ce718ad413fb388b88b80767c6c0
Domen Kožar <domen@dev.si>
2023-09-27 14:32:39 +0100

    aarch64-linux: 1G /boot

--- disko-hetzner-cloud.nix
+++ disko-hetzner-cloud.nix
@@ -9,7 +9,7 @@
           partitions = {
             ESP =  {
               type = "EF00";
-              size = "100M";
+              size = "1000M";
               content = {
                 type = "filesystem";
                 format = "vfat";
