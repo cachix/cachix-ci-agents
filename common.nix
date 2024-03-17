cdb0d39ad8d4a0335289cd228b0fde60b65abd97
Domen Kožar <domen@dev.si>
2024-03-17 03:51:40 +0000

    upgrade nix, limit cores

--- common.nix
+++ common.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }:
 
 {
-  nix.package = pkgs.nixVersions.nix_2_16;
+  nix.settings.cores = 4;
   nix.extraOptions = ''
     min-free = ${toString (5 * 1024 * 1024 * 1024)}
     max-free = ${toString (20 * 1024 * 1024 * 1024)}
   '';
-
   environment.systemPackages = with pkgs; [ 
     vim 
     # zig broken on darwin
