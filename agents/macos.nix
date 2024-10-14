406af4858fcd5d732a22b6fcc66b500de3cd7716
Sander <hey@sandydoo.me>
2024-10-14 13:31:55 +0000

    macos: disable nixbld check

--- agents/macos.nix
+++ agents/macos.nix
@@ -21,6 +21,9 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
+  # Disable check for nixbld uid range.
+  ids.uids.nixbld = 300;
+
   # for some reason manual isn't reproducible so we disable it
   documentation.man.enable = lib.mkForce false;
   documentation.info.enable = lib.mkForce false;
