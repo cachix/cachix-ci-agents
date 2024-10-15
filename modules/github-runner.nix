bb321d7b1f1dcea70f8c005454bca0cf711ee3d2
Sander <hey@sandydoo.me>
2024-10-15 01:21:54 +0000

    runner: update pre-job script

--- modules/github-runner.nix
+++ modules/github-runner.nix
@@ -8,8 +8,7 @@
 let
   cfg = config.cachix.github-runner;
   preJobScript = pkgs.writeShellScript "runner-pre-job.sh" ''
-    #!/usr/bin/env bash
-    nixconf=$(mktemp "nix.conf.XXXXXX" --tmpdir $RUNNER_TEMP)
+    nixconf=$(${pkgs.coreutils}/bin/mktemp --tmpdir $RUNNER_TEMP "nix.conf.XXXXXX")
     echo "access-tokens = $GITHUB_TOKEN" >> $nixconf
     echo "NIX_USER_CONF_FILES=$(readlink -f "$nixconf")" >> $GITHUB_ENV
   '';
